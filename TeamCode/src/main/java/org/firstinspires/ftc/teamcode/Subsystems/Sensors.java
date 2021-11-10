f17ed7880fca34d5f1f4bcb9b7422694c4de8119
Phil Szymanowski <FTC14133@gmail.com>
2021-11-10 16:31:50 -0500

    Need to change digital channel callouts. Each channel has 2 states. We want an Alliance switch and a start position switch, which is 2*2=4 different auto options.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Sensors.java
@@ -4,9 +4,10 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Sensors {
-    DigitalChannel Red; //Sets the varible of a switch on the robot
-    DigitalChannel Blue; //Sets the varible of a switch on the robot
-    public int Red_Blue; // Makes a varible to find the differnce between the red or blue switches
+    DigitalChannel Red; //Sets the varible of a switch on the robot fixme: one input should be for alliance side and another for warehouse or turntable start. Call this one alliance switch.
+    DigitalChannel Blue; //Sets the varible of a switch on the robot fixme: red/blue is the determination from the switches. In other words, one switch has 2 different states, on (red) or off (blue)
+   //fixme: The other digital channel should be field side - on=warehouse off=turntable.
+    public int Red_Blue; // Makes a varible to find the differnce between the red or blue switches fixme: call this Alliance instead
 
     public Sensors(HardwareMap hardwareMap){
         Red = hardwareMap.get(DigitalChannel.class, "R"); //Defines the switches on the robot
@@ -15,10 +16,14 @@ public class Sensors {
 
     public int Update_Red_Blue(){
         if (Red.getState()) { //Gets the state of the red switch
-            Red_Blue = 1;
+            Red_Blue = 1; // fixme: this logic changes slightly:
+            // if(alliance.getState()
+            //alliance=blue
+            //else
+            //alliance=red
         }
         if (Blue.getState()) { //Gets the state of the red switch
-            Red_Blue = 0;
+            Red_Blue = 0; //fixme: same thing here
         }
         return Red_Blue;
     }
