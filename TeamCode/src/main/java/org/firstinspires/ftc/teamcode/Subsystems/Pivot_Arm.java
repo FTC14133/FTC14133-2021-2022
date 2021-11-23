9a3bb789a730d9c2e8b29c80091a1f782653b9c7
A-volpe107 <pandafox2319@gmail.com>
2021-11-22 17:41:48 -0800

    fixed pivot arm errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Pivot_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Pivot_Arm.java
@@ -13,43 +13,16 @@ public class Pivot_Arm {
     private DcMotorEx lift;
     public int position = 0; // Integer position of the arm
 
-    {switch (position) {
-        case 0:
-            System.out.println("Position=0");
-                    break;
-        case 1:
-            System.out.println("Position=1");
-                  break;
-        case 2:
-            System.out.println("Position=2");
-                    break;
-        case 3:
-            System.out.println("Position=3");
-                    break;
 
 
 
-    }
+
     //ToDo: Add encoder count amount (still undetermined, just put a number in now)
     //ToDo: Add stop and reset encoder in init(), and run using encoder. See FTC_14133_2021_Auto.java
     //ToDo: Add limit switch sensing for either extreme location and to reset to 0 on one side (Homing) or in the middle
     //ToDo: Create set-point positions for different arm positions (Intake, place low, place mid, place high, different sides)
         //This can likely be done with looking at different states and using the arrows, bumpers, or something else to move up the integer positions
         //Pseudo code:
-             int position == 0; //initial position of arm
-             if (gamepad2.dpad_up==1) {
-                 position = position + 1
-             }
-             else if(gamepad.dpad_down==1)
-            // position = position - 1}
-
-            // if (position == 0){
-            // set encoder position to x
-            // else if(position==1){
-            // set encoder position to y //etc}
-            // else if(position==-1){
-            // set encoder position to -y //etc}
-            //}
 
     public Pivot_Arm(HardwareMap hardwareMap){                 // Motor Mapping
         lift = hardwareMap.get(DcMotorEx.class, "lift");      //Sets the names of the hardware on the hardware map
@@ -60,14 +33,15 @@ public class Pivot_Arm {
         position=0; //initial arm position
     }
 
-    public void Pivot_Arm_Telop(Gamepad gamepad2){ //Code to be run in Op Mode void Loop at top level
+    public int Pivot_Arm_Telop(Gamepad gamepad2){ //Code to be run in Op Mode void Loop at top level
 
-                     integer position == 0; //initial position of arm
-                     if (gamepad.dpadup==1){
-                         position = position + 1
-             else if(gamepad.dpaddown==1)
 
-                             int position = 0; //Initial position of arm
+                     if (gamepad2.dpad_up==true) {
+                         position = position + 1;
+                     }
+                     else if(gamepad2.dpad_down==true)
+
+
 
                          switch (position) {
                              case 0:
@@ -98,7 +72,7 @@ public class Pivot_Arm {
         //}
 
     }
-    public int getArmPosition(){
+     int getArmPosition;{
         return position;
     }
-}
+}}
