e55de85e2688b586b747888b34965cbbda8ed99e
FTC14133 <FTC14133@gmail.com>
2021-11-23 01:37:25 -0500

    Fixed formatting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -14,7 +14,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Intake {
     // Instantiate the motor variables
     private DcMotorEx intake;
-    int pos= 0 ;
     boolean toggle = true;
     boolean Possession;
     DigitalChannel beambreak; //The "beambreak" sensor is a type of IR sensor that detects if it vision is broken
@@ -26,43 +25,32 @@ public class Intake {
         // Set motor direction based on which side of the robot the motors are on
         intake.setDirection(DcMotorEx.Direction.FORWARD);
     }
-    
-    /* JAKE- send the least amount of data possible for instance
-    becuase the life only requires the use of an int (which is a primative type)
-    you can just send the int. 
 
-    My General Rule of thumb is if you need to less than 2 members from an object over send them individually (and make sure your argument names are declarative);
-    if you need to access 2 or more members just send the entire object over and use that to get your required values
-
-    if you need to access logic from an object ie a method that raises the arm
-    */
-    public void Update_intake(int speed, int position){ //todo: rather than having one method try to do everything in auto, make multiple methods like we did last year that each do something individually. Much easier to keep track of.
-        if(position>0){
-            speed = -speed;
+    public void Update_intake(double speed, int position){ //todo: rather than having one method try to do everything in auto, make multiple methods like we did last year that each do something individually. Much easier to keep track of.
+        if(position<0){ //if the arm is towards the back
+            speed = -speed; //flip the direction of the intake
         }
         if(!beambreak.getState()) { //if beam is broken
-            Possession = true;
+            Possession = true; //we have possession
             intake.setPower(0);//Stop intake
-
-
         }
         else{ // if beam break not broken
-            Possession = false;
+            Possession = false; //we do not have possession
             intake.setPower(speed); // Run intake
         }
-
     }
 
-    public void Update_outtake(int speed){ //todo: rather than having one method try to do everything in auto, make multiple methods like we did last year that each do something individually. Much easier to keep track of.
-
-        intake.setPower(speed);
-        if() { //if beam is broken
-
-
+    public void Update_outtake(double speed, int position){
+        if(position<0){
+            speed=-speed;
+            intake.setPower(speed);
+        }
+        else{
+            intake.setPower(0);
         }
 
     }
-    public void Update_telop(Gamepad gamepad2, int liftPosition){ //Code to be run in Op Mode void Loop at top level
+    public void Update_telop(Gamepad gamepad2, int position){ //Code to be run in Op Mode void Loop at top level
 
         if (gamepad2.y){
             intake.setPower(0.25);
@@ -71,36 +59,6 @@ public class Intake {
         if (gamepad2.b){
             intake.setPower(-0.25);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            // gets the current arm position
-        /*if (gamepad2.right_trigger > 0 && liftPosition>0 ) {       //runs the intake forward based on arm position
-            intake.setPower(1);
-        }else if (gamepad2.right_trigger > 0 && liftPosition<0){
-            intake.setPower(-1);                           // runs the intake forward, but the arm is backwards so it is negative
-        }
-
-        else{
-            intake.setPower(0);
-        }
-
-         */
-
-
 
     }
 
