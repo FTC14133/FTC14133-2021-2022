102e4e075f48ede303129df46a7c090fc9c4f80e
A-volpe107 <pandafox2319@gmail.com>
2021-12-08 20:40:08 -0500

    made gear ratio and lift angle easier to change
    cool beans

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Pivot_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Pivot_Arm.java
@@ -18,8 +18,12 @@ public class Pivot_Arm {
     DigitalChannel HomeSwitch;
 
     public int position = 0; // Integer position of the arm
-
-
+    int tolerance = 2; // Encoder tolerance
+    final double countsperrev = 28; // Counts per rev of the motor
+    final double gearratio=3*4*5*4; //Ratio of the entire Pivot Arm from the motor to the arm
+    final double countsperdegree=countsperrev*gearratio/360;
+    final int countsperdegreeint=(int)countsperdegree;
+    final double liftpower=0.5;
 
 
 
@@ -64,49 +68,49 @@ public class Pivot_Arm {
         switch (position) {
             case -3: // Intake Back
                 System.out.println("Position=-3");
-                lift.setTargetPosition(0); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(0*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
 
             case -2: // Mid Level back
                 System.out.println("Position=-2");
-                lift.setTargetPosition(186); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(34*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
 
             case -1: //Upper Level Back
                 System.out.println("Position=-1");
-                lift.setTargetPosition(372); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(75*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
 
             case 0: //Straight Up
                 System.out.println("Position=0");
-                lift.setTargetPosition(558); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(120*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
 
             case 1: //Upper Level Front
                 System.out.println("Position=1");
-                lift.setTargetPosition(744); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(170*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
 
             case 2: //Mid Level Front
                 System.out.println("Position=2");
-                lift.setTargetPosition(930); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(210*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
             case 3:
                 System.out.println("Position=3");
-                lift.setTargetPosition(1120); //Todo: Need to tune
-                lift.setPower(0.25);        //Sets the power for the lift
+                lift.setTargetPosition(265*countsperdegreeint); //Todo: Need to tune
+                lift.setPower(liftpower);        //Sets the power for the lift
                 lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION); //Allows the lift to run
                 break;
             default:
